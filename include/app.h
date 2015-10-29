cf5f079ff324a9ccd4f69b477dfaea5e621350be
kamleshbambarde <kamlesh.bambarde@outlook.com>
2015-10-29 18:16:09 +0530

    .

--- include/app.h
+++ include/app.h
@@ -6,7 +6,7 @@
 #define PIGEON_APP_ENV_H
 
 #include <settings.h>
-
+#include <http_filter_base.h>
 
 using namespace std;
 
@@ -19,6 +19,8 @@ namespace pigeon {
     public:
 
         auto run() -> void;
+		auto add_http_filter(http_filter_base*) -> void;
+
 
     };
 
