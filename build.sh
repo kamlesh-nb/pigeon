9749e33c0c310f4020216864869f36d365212e7f
kamlesh.bambarde <kamlesh.bambarde@outlook.com>
2016-05-08 11:17:17 +0530

    added shell script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Environment: `uname -a`"
-echo "Compiler: `$CXX --version`"
+echo "Environment: 'uname -a'"
+echo "Compiler: '$CXX --version'"
 
 make || exit 1
 make test || exit 1
